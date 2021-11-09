3673dea8fc6420031b3a13076689c33f6843f20d
Sebastian Soto Madrigal <s.m.sebastian.n@gmail.com>
2021-11-09 16:49:16 -0600

    adjustPan when input focused, disable button when no username

--- app/src/main/java/com/example/palfinder/views/auth/recover/password/GetRecoveryCodeFragment.kt
+++ app/src/main/java/com/example/palfinder/views/auth/recover/password/GetRecoveryCodeFragment.kt
@@ -1,11 +1,14 @@
 package com.example.palfinder.views.auth.recover.password
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.palfinder.R
+import kotlinx.android.synthetic.main.fragment_get_recovery_code.view.*
 
 class GetRecoveryCodeFragment : Fragment() {
 
@@ -13,6 +16,15 @@ class GetRecoveryCodeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_get_recovery_code, container, false)
+        val view = inflater.inflate(R.layout.fragment_get_recovery_code, container, false)
+        view.etUsername?.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun afterTextChanged(p0: Editable?) {}
+
+            override fun onTextChanged(text: CharSequence?, start: Int, before: Int, count: Int) {
+                view.btnSend?.isEnabled = text.toString().isNotBlank()
+            }
+        })
+        return view
     }
 }
