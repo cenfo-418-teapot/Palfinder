036317c2754e72a015ff45344ec97d5d072513e3
Sebastian Soto Madrigal <s.m.sebastian.n@gmail.com>
2021-11-09 17:49:06 -0600

    Functional password recovery

--- app/src/main/java/com/example/palfinder/views/auth/recover/password/RecoverPasswordActivity.kt
+++ app/src/main/java/com/example/palfinder/views/auth/recover/password/RecoverPasswordActivity.kt
@@ -1,13 +1,16 @@
 package com.example.palfinder.views.auth.recover.password
 
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager2.widget.ViewPager2
 import com.example.palfinder.R
 import kotlinx.android.synthetic.main.activity_recover_password.*
 
-class RecoverPasswordActivity : AppCompatActivity(R.layout.activity_recover_password) {
+interface OnStepChange {
+    fun onStepChange(step: Int)
+}
+
+class RecoverPasswordActivity : AppCompatActivity(R.layout.activity_recover_password), OnStepChange {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val steps = arrayListOf("Get Verification Code", "Change Password")
@@ -16,12 +19,13 @@ class RecoverPasswordActivity : AppCompatActivity(R.layout.activity_recover_pass
             .stepsNumber(steps.size)
             .commit()
         setupViewPager()
-        svRecoverySteps.setOnStepClickListener { position ->
-            vpRecoverySteps.setCurrentItem(
-                position,
-                false
-            )
-        }
+//        Uncomment to enable manual motion with clicks on stepper
+//        svRecoverySteps.setOnStepClickListener { position ->
+//            vpRecoverySteps.setCurrentItem(
+//                position,
+//                false
+//            )
+//        }
     }
 
     private fun setupViewPager() {
@@ -33,5 +37,9 @@ class RecoverPasswordActivity : AppCompatActivity(R.layout.activity_recover_pass
                 svRecoverySteps.go(position, true)
             }
         })
+    }
+
+    override fun onStepChange(step: Int) {
+       vpRecoverySteps.setCurrentItem(step, false)
     }
 }
