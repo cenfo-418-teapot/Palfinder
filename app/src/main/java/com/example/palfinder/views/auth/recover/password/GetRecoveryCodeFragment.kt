a7d1c6c28a6ae762641ad019f01dad3556a36a68
Sebastian Soto Madrigal <s.m.sebastian.n@gmail.com>
2021-11-09 17:49:06 -0600

    Functional password recovery

--- app/src/main/java/com/example/palfinder/views/auth/recover/password/GetRecoveryCodeFragment.kt
+++ app/src/main/java/com/example/palfinder/views/auth/recover/password/GetRecoveryCodeFragment.kt
@@ -1,16 +1,38 @@
 package com.example.palfinder.views.auth.recover.password
 
+import android.content.Context
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.MutableLiveData
+import com.amplifyframework.auth.AuthException
 import com.example.palfinder.R
+import com.example.palfinder.backend.services.AuthenticationService
+import kotlinx.android.synthetic.main.fragment_get_recovery_code.*
+import kotlinx.android.synthetic.main.fragment_get_recovery_code.etUsername
+import kotlinx.android.synthetic.main.fragment_get_recovery_code.tilUsername
 import kotlinx.android.synthetic.main.fragment_get_recovery_code.view.*
+import kotlinx.android.synthetic.main.sign_up_fragment.*
 
 class GetRecoveryCodeFragment : Fragment() {
+    lateinit var stepChange: OnStepChange
+    private val _userFound = MutableLiveData<Boolean>()
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        stepChange = context as OnStepChange
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _userFound.observe(this, {
+            tilUsername?.error = if (!it) "That user does not exist" else null
+        })
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,6 +47,26 @@ class GetRecoveryCodeFragment : Fragment() {
                 view.btnSend?.isEnabled = text.toString().isNotBlank()
             }
         })
+        view.tvHasCode?.setOnClickListener {
+            stepChange.onStepChange(1)
+        }
+        view.btnSend?.setOnClickListener {
+            AuthenticationService.PasswordReset.sendCode(
+                etUsername.text.toString(),
+                {
+                    _userFound.postValue(true)
+                    stepChange.onStepChange(1)
+                }, {
+                    when (it) {
+                        is AuthException.UserNotFoundException -> _userFound.postValue(false)
+                        else -> Log.e(TAG, "Password reset failed", it)
+                    }
+                }
+            )
+        }
         return view
+    }
+    companion object {
+        private const val TAG = "GetRecoveryCodeFragment"
     }
 }
