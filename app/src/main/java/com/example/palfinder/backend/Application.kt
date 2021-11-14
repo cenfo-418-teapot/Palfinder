f3d6caa0a1ce56548631067efeb8341ee711a099
istaack <isaac@trtinteractive.com>
2021-11-14 02:00:08 -0600

    build: Enabling Amplify API

--- app/src/main/java/com/example/palfinder/backend/Application.kt
+++ app/src/main/java/com/example/palfinder/backend/Application.kt
@@ -3,6 +3,7 @@ package com.example.palfinder.backend
 import android.app.Application
 import android.util.Log
 import com.amplifyframework.AmplifyException
+import com.amplifyframework.api.aws.AWSApiPlugin
 import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin
 import com.amplifyframework.core.Amplify
 
@@ -11,6 +12,7 @@ class ApplicationInitializer : Application() {
         super.onCreate()
         try {
             Amplify.addPlugin(AWSCognitoAuthPlugin())
+            Amplify.addPlugin(AWSApiPlugin())
             Amplify.configure(applicationContext)
             Log.i(TAG, "Initialized Amplify")
         } catch (error: AmplifyException) {
