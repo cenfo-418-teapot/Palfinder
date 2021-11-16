357dfbb8461efda67eac1c1aa0738a0ba6d6e5d9
iStaack <isaac@trtinteractive.com>
2021-11-16 16:06:18 -0600

    refactor: Setting up group form

--- app/src/main/java/com/example/palfinder/views/groups/GroupEditFragment.kt
+++ app/src/main/java/com/example/palfinder/views/groups/GroupEditFragment.kt
@@ -5,12 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.amplifyframework.datastore.generated.model.State
 import com.example.palfinder.R
+import com.example.palfinder.views.auth.SignUpFragment
+import kotlinx.android.synthetic.main.fragment_group_edit.view.*
+import kotlinx.android.synthetic.main.fragment_sign_in.view.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+//// TODO: Rename parameter arguments, choose names that match
+//// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+//private const val ARG_PARAM1 = "param1"
+//private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -24,10 +29,10 @@ class GroupEditFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+//        arguments?.let {
+//            param1 = it.getString(ARG_PARAM1)
+//            param2 = it.getString(ARG_PARAM2)
+//        }
     }
 
     override fun onCreateView(
@@ -35,26 +40,44 @@ class GroupEditFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_group_edit, container, false)
+        val view = inflater.inflate(R.layout.fragment_group_edit, container, false)
+        view.btnCancel?.setOnClickListener {
+            goTo(view, R.id.action_groupEditFragment_to_groupListFragment)
+        }
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment GroupEditFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            GroupEditFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun goTo(tmpView: View, tmpIdElement: Int) {
+        Navigation.findNavController(tmpView).navigate(tmpIdElement)
     }
+
+//    private fun validForm(): GroupForm {
+//
+//    }
+
+    private data class GroupForm(
+            val name: String,
+            val description: String,
+            val state: State,
+            var imageName: String? = null)
+
+//    companion object {
+//        /**
+//         * Use this factory method to create a new instance of
+//         * this fragment using the provided parameters.
+//         *
+//         * @param param1 Parameter 1.
+//         * @param param2 Parameter 2.
+//         * @return A new instance of fragment GroupEditFragment.
+//         */
+//        // TODO: Rename and change types and number of parameters
+//        @JvmStatic
+//        fun newInstance(param1: String, param2: String) =
+//            GroupEditFragment().apply {
+//                arguments = Bundle().apply {
+//                    putString(ARG_PARAM1, param1)
+//                    putString(ARG_PARAM2, param2)
+//                }
+//            }
+//    }
 }
