319638c2f26f4c112d340ec892470724733f1a6d
Sebastian Soto Madrigal <s.m.sebastian.n@gmail.com>
2021-12-04 18:43:50 -0600

    working initial setup (tags & groups)

--- app/src/main/java/com/example/palfinder/views/user/account/InitialSetupConfirmationFragment.kt
+++ app/src/main/java/com/example/palfinder/views/user/account/InitialSetupConfirmationFragment.kt
@@ -1,34 +1,38 @@
 package com.example.palfinder.views.user.account
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ProgressBar
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.MutableLiveData
 import com.amplifyframework.api.graphql.model.ModelMutation
 import com.amplifyframework.api.graphql.model.ModelQuery
 import com.amplifyframework.core.Amplify
-import com.amplifyframework.datastore.generated.model.Tag
-import com.amplifyframework.datastore.generated.model.TagStatus
-import com.amplifyframework.datastore.generated.model.TagUser
-import com.amplifyframework.datastore.generated.model.User
+import com.amplifyframework.datastore.generated.model.*
 import com.example.palfinder.R
+import com.example.palfinder.backend.services.GroupAdmin
 import com.example.palfinder.backend.services.InitialSetupData
 import com.example.palfinder.backend.services.UserData
+import com.example.palfinder.views.HomeActivity
 import com.google.android.material.chip.Chip
 import com.google.android.material.chip.ChipGroup
 import kotlinx.android.synthetic.main.fragment_initial_setup_confirmation.view.*
 
 class InitialSetupConfirmationFragment : Fragment() {
     private var currentUser = UserData.currentUser.value!!
+    private val _groupsToAdd = ArrayList<Group>()
+    private val groupsToAddLiveData = MutableLiveData<List<GroupAdmin.GroupModel>>()
     private val _tagsToAdd = ArrayList<Tag>()
     private val tagsToAddLiveData = MutableLiveData<List<Tag>>()
-    private val progressLiveData = MutableLiveData<Int>()
+    private val progressLiveData = MutableLiveData<String>()
     private var _finishedAdditions = 0
     private var _totalChanges = 0
+    private lateinit var progressBar: ProgressBar
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -36,26 +40,95 @@ class InitialSetupConfirmationFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_initial_setup_confirmation, container, false)
-        progressLiveData.observe(viewLifecycleOwner, {
-            Log.i(TAG, "Finished an action")
+        progressBar = requireActivity().findViewById(R.id.progressBar2)
+        progressLiveData.observe(viewLifecycleOwner, { action ->
+            Log.i(TAG, "Finished an action | $action")
             _finishedAdditions++
-            view.progressBar2.progress = _finishedAdditions
-//            if (totalAdditions == _finishedAdditions) startActivity(Intent(activity, HomeActivity::class.java))
+            progressBar.progress = _finishedAdditions
+            if (_totalChanges == _finishedAdditions) {
+                Amplify.API.query(ModelQuery.get(User::class.java, currentUser.id),
+                    { userResponse ->
+                        val user = userResponse.data
+                        val updatedUser = User.builder()
+                            .email(user.email)
+                            .username(user.username)
+                            .name(user.name)
+                            .lastName(user.lastName)
+                            .status(UserStatus.COMPLETE)
+                            .description(user.description)
+                            .phoneNumber(user.phoneNumber)
+                            .photo(user.photo)
+                            .id(user.id)
+                            .build()
+                        Amplify.API.mutate(
+                            ModelMutation.update(updatedUser),
+                            { updatedUserResponse ->
+                                UserData.setCurrentUser(updatedUserResponse.data)
+                                startActivity(Intent(activity, HomeActivity::class.java))
+                            },
+                            { Log.e(TAG, "Failed to change user status", it) })
+                    },
+                    { Log.e(TAG, "Failed to get user from database", it) })
+            }
         })
-        observeTagsList(view)
-        observeGroupsList(view)
-        observeTagsToAdd()
+        observeTagsAdditionFlow(view)
+        observeGroupsAdditionFlow(view)
         requireActivity().findViewById<Button>(R.id.btnSave).setOnClickListener { _ ->
             val totalTagsToAdd = InitialSetupData.tagsList.value?.size ?: 0
             val totalGroupsToAdd = InitialSetupData.groupsList.value?.size ?: 0
             _totalChanges = totalTagsToAdd + totalGroupsToAdd
-            if (view.progressBar2.visibility == View.GONE && _totalChanges > 0) {
-                view.progressBar2.visibility = View.VISIBLE
-                view.progressBar2.max = _totalChanges
+            if (progressBar.visibility == View.GONE && _totalChanges > 0) {
+                progressBar.visibility = View.VISIBLE
+                progressBar.max = _totalChanges
             }
             InitialSetupData.tagsList.value?.forEach { createTag(it) }
+            groupsToAddLiveData.postValue(InitialSetupData.groupsList.value)
         }
         return view
+    }
+
+    private fun observeGroupsAdditionFlow(view: View) {
+        observeGroupsList(view)
+        observeGroupsToAdd()
+    }
+
+    private fun observeTagsAdditionFlow(view: View) {
+        observeTagsList(view)
+        observeTagsToAdd()
+    }
+
+    private fun observeGroupsToAdd() {
+        groupsToAddLiveData.observe(viewLifecycleOwner, { groupsToAdd ->
+            if (groupsToAdd.size == InitialSetupData.groupsList.value!!.size) {
+                Amplify.API.query(ModelQuery.get(User::class.java, currentUser.id),
+                    {
+                        UserData.setCurrentUser(it.data)
+                        currentUser = it.data
+                        groupsToAdd.forEach { group -> addGroupToUser(currentUser.groups, group) }
+                    },
+                    { Log.e(TAG, "Couldn't fetch the user's groups", it) })
+            }
+        })
+    }
+
+    private fun addGroupToUser(currentGroups: List<GroupMembers>?, group: GroupAdmin.GroupModel) {
+        val userIsMember = currentGroups?.find { member -> member.group.id == group.id }
+        if (userIsMember != null) {
+            progressLiveData.postValue("User was already a member of: ${group.name}")
+        } else {
+            val member = GroupMembers.builder().user(currentUser).group(group.data).build()
+            Amplify.API.mutate(ModelMutation.create(member),
+                {
+                    currentUser.groups.add(it.data)
+                    progressLiveData.postValue("${currentUser.username} added to group ${group.name}")
+                },
+                {
+                    Log.e(
+                        TAG,
+                        "${currentUser.username} was not added as a member to ${group.name}"
+                    )
+                })
+        }
     }
 
     private fun observeTagsToAdd() {
@@ -87,14 +160,13 @@ class InitialSetupConfirmationFragment : Fragment() {
                 { response ->
                     currentUser.tags.add(response.data)
                     Log.i(TAG, currentUser.tags.toString())
-                    progressLiveData.postValue(1)
+                    progressLiveData.postValue("Asigned ${tag.name} to user")
                 },
                 { error ->
                     Log.e(TAG, "Couldn't assign ${tag.name} to ${currentUser.name}", error)
                 })
         } else {
-            Log.i(TAG, "This user already has the tag ${tag.name}")
-            progressLiveData.postValue(1)
+            progressLiveData.postValue("AddTagToUser | User already has tag ${tag.name}")
         }
     }
 
@@ -124,6 +196,9 @@ class InitialSetupConfirmationFragment : Fragment() {
                         view.cgSelectedTags.removeView(chp)
                     }
                 }
+            } else {
+                view.cgSelectedTags.visibility = View.GONE
+                view.emptyTagSelection.visibility = View.VISIBLE
             }
         })
     }
@@ -152,7 +227,7 @@ class InitialSetupConfirmationFragment : Fragment() {
             if (it != null) {
                 val userHasTag = currentUser.tags?.find { uTag -> uTag.tag.id == it.id } != null
                 if (userHasTag) {
-                    Log.i(TAG, "User already has tag $name")
+                    progressLiveData.postValue("User already has tag $name")
                 } else {
                     var updatedTag = Tag.builder()
                         .name(it.name)
@@ -198,6 +273,10 @@ class InitialSetupConfirmationFragment : Fragment() {
         if (!exists) {
             val chip = layoutInflater.inflate(R.layout.selected_tag, chipGroup, false) as Chip
             chip.text = name
+            chip.setOnCloseIconClickListener {
+                InitialSetupData.setTagsList(InitialSetupData.tagsList.value!!.filter { tag -> tag != name }
+                    .toList())
+            }
             chipGroup.addView(chip)
             Log.i(TAG, "Created chip $name")
             return chip
