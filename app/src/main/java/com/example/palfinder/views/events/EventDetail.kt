91edf0ecb3415417f2b1c8d9137bee5959895480
Josué B <jbrenesr1@ucenfotec.ac.cr>
2021-12-10 17:51:36 -0600

    Update EventDetail.kt

--- app/src/main/java/com/example/palfinder/views/events/EventDetail.kt
+++ app/src/main/java/com/example/palfinder/views/events/EventDetail.kt
@@ -6,10 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.MutableLiveData
 import androidx.navigation.navGraphViewModels
+import com.amplifyframework.api.graphql.model.ModelQuery
+import com.amplifyframework.core.Amplify
 import com.amplifyframework.datastore.generated.model.Event
+import com.amplifyframework.datastore.generated.model.User
 import com.example.palfinder.R
 import com.example.palfinder.backend.services.event.EventAPI
+import com.example.palfinder.views.search.UserItemAdapter
 import com.example.palfiner.backend.services.event.EventManager
 import kotlinx.android.synthetic.main.fragment_event_create.view.*
 
@@ -19,19 +24,18 @@ class EventDetail : Fragment(){
 
     private var  id :String? = null
 
-    var event : Any? = null
+    private val event = MutableLiveData<Event>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-             id = it.getString(ARG_ID)
+            id = it.getString(ARG_ID)
             Log.i("Prueba", id.toString())
-
-            event = EventAPI.getEventById(id.toString())
-            Log.i("Prueba", event.toString())
         }
 
 
+
+        get(id.toString())
     }
 
     override fun onCreateView(
@@ -42,8 +46,34 @@ class EventDetail : Fragment(){
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_event_detail, container, false)
 
+        event.observe(viewLifecycleOwner, { event ->
+
+            Log.i("Event Mnager ", event.toString())
+        })
 
         return view
+    }
+
+    fun get(id : String) {
+
+        Amplify.API.query(
+            ModelQuery.get(Event::class.java, id.toString()),
+            { response ->
+                Log.i("Event Mnager ", "Created")
+                if (response.hasErrors()) {
+                    Log.e("Event Mnager ", response.errors.first().message)
+                } else {
+                    Log.i("Event Mnager ", "Updated  Event with id: " + response.data.id)
+
+                    if (response.hasData()) {
+                        event.postValue(response.data as Event)
+                    }
+                }
+            },
+            { error ->
+                Log.e("Event Mnager ", "Update failed", error)
+            })
+
     }
 
 
@@ -60,6 +90,6 @@ companion object{
         }
 
 }
-}
+    }
 
 
