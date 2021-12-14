985bf7c0b15c58003738ec07c4fb10bb09c8b6b0
Sebastian Soto Madrigal <s.m.sebastian.n@gmail.com>
2021-12-13 21:14:10 -0600

    checkpoint a la carrera para josue

--- app/src/main/java/com/example/palfinder/views/events/EventDetailActivity.kt
+++ app/src/main/java/com/example/palfinder/views/events/EventDetailActivity.kt
@@ -1,19 +1,18 @@
 package com.example.palfinder.views.events
 
-import android.content.Intent
 import android.icu.text.SimpleDateFormat
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.forEach
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.MutableLiveData
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
@@ -21,13 +20,7 @@ import com.amplifyframework.api.graphql.model.ModelQuery
 import com.amplifyframework.core.Amplify
 import com.amplifyframework.datastore.generated.model.Event
 import com.example.palfinder.R
-import com.example.palfinder.backend.services.UserData
 import com.example.palfinder.backend.services.event.EventData
-import com.example.palfinder.views.HomeActivity
-import com.example.palfinder.views.search.FiltersModal
-import com.example.palfinder.views.search.SearchEventsFragment
-import com.example.palfinder.views.search.SearchGroupsFragment
-import com.example.palfinder.views.search.SearchUsersFragment
 import kotlinx.android.synthetic.main.activity_event_detail.*
 import kotlinx.android.synthetic.main.activity_home.*
 import kotlinx.android.synthetic.main.activity_search.*
@@ -41,14 +34,12 @@ private const val ARG_ID = "id"
 
 class EventDetailActivity : AppCompatActivity(R.layout.activity_event_detail) {
 
-    private var  id :String? = null
+    private var id: String? = null
 
     private val event = MutableLiveData<Event>()
 
     private val steps = arrayListOf(
-        Pair(R.id.mnuUsers, SearchUsersFragment()),
-        Pair(R.id.mnuEvents, SearchEventsFragment()),
-        Pair(R.id.mnuGroups, SearchGroupsFragment()),
+        Pair(R.id.mnuUsers, EventDetailFragment()),
     )
 
     @RequiresApi(Build.VERSION_CODES.O)
@@ -56,18 +47,13 @@ class EventDetailActivity : AppCompatActivity(R.layout.activity_event_detail) {
         super.onCreate(savedInstanceState)
         EventData.currentEvent.observe(this, {
             eventTittle.text = it.name
-            val pattern = "dd MMMM yyyy HH:mm a"
-            val simpleDateFormat = SimpleDateFormat(pattern)
-            val date: String = simpleDateFormat.format(it.`when`.toDate())
-            evetDate.text = date.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL))
-            eventDetailDescription.text = it.description
             Log.i("Event Mnager ", it.toString())
         })
     }
 
 
-     @RequiresApi(Build.VERSION_CODES.O)
-     fun onCreateView(
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -76,11 +62,12 @@ class EventDetailActivity : AppCompatActivity(R.layout.activity_event_detail) {
         val view = inflater.inflate(R.layout.fragment_event_detail, container, false)
         val event = EventData.currentEvent.value
         Log.i("Event detail", event.toString())
-
+        setupViewPager()
+        setupNavigation()
         return view
     }
 
-    fun get(id : String) {
+    fun get(id: String) {
 
         Amplify.API.query(
             ModelQuery.get(Event::class.java, id.toString()),
@@ -103,12 +90,64 @@ class EventDetailActivity : AppCompatActivity(R.layout.activity_event_detail) {
     }
 
 
-    fun newInstance(id : String) =
+    fun newInstance(id: String) =
         EventDetail().apply {
             arguments = Bundle().apply {
                 putString(ARG_ID, id)
             }
         }
 
+    private fun setupNavigation() {
+        toggleNavigationButton(steps[0].first)
+        bottom_navigation.setOnItemSelectedListener { item ->
+            toggleNavigationButton(item.itemId)
+            vpEventFragments.setCurrentItem(
+                steps.map { data -> data.first }.indexOf(item.itemId),
+                true
+            )
+            true
+        }
+    }
+
+    private fun toggleNavigationButton(id: Int) {
+        bottom_navigation.menu.forEach {
+            val isCurrent = it.itemId == id
+            it.isEnabled = !isCurrent
+            if (isCurrent) supportActionBar?.title = it.title
+        }
+    }
+
+    private fun setupViewPager() {
+        vpEventFragments.adapter = ViewPagerAdapter(
+            steps.map { data -> data.second } as ArrayList<Fragment>,
+            supportFragmentManager,
+            lifecycle
+        )
+//        vpEventFragments.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+//            override fun onPageSelected(position: Int) {
+//                super.onPageSelected(position)
+//                bottom_navigation.selectedItemId = steps.map { data -> data.first }[position]
+//            }
+//        })
+        vpEventFragments.isUserInputEnabled = false
+    }
+
+    inner class ViewPagerAdapter(
+        list: ArrayList<Fragment>,
+        fm: FragmentManager,
+        lifecycle: Lifecycle
+    ) : FragmentStateAdapter(fm, lifecycle) {
+
+        private val fragmentList = list
+
+        override fun getItemCount(): Int {
+            return fragmentList.size
+        }
+
+        override fun createFragment(position: Int): Fragment {
+            return fragmentList[position]
+        }
+    }
 }
+
 
