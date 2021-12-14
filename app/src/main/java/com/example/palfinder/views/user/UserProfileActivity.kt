2d479e1120070c745425f638e4d8ae6aebf51cc9
Mauricio Poveda <mauricio.poveda@gmail.com>
2021-12-13 23:25:59 -0600

    Add avatar image from device gallery (#42)

--- app/src/main/java/com/example/palfinder/views/user/UserProfileActivity.kt
+++ app/src/main/java/com/example/palfinder/views/user/UserProfileActivity.kt
@@ -1,5 +1,7 @@
 package com.example.palfinder.views.user
 
+import android.R.attr
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.amplifyframework.api.graphql.model.ModelQuery
@@ -7,6 +9,10 @@ import com.amplifyframework.core.Amplify
 import com.amplifyframework.datastore.generated.model.User
 import com.example.palfinder.R
 import com.example.palfinder.backend.services.UserData
+import android.R.attr.data
+import android.app.Activity
+import android.net.Uri
+
 
 class UserProfileActivity : AppCompatActivity(R.layout.activity_user_profile) {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/palfinder/views/user/UserProfileEditFragment.kt
+++ app/src/main/java/com/example/palfinder/views/user/UserProfileEditFragment.kt
@@ -1,11 +1,18 @@
 package com.example.palfinder.views.user
 
+import android.R.attr
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import com.amplifyframework.core.Amplify
@@ -16,13 +23,39 @@ import com.example.palfinder.R
 import com.example.palfinder.backend.services.UserData
 import kotlinx.android.synthetic.main.fragment_user_profile_edit.*
 import kotlinx.android.synthetic.main.fragment_user_profile_edit.view.*
+import java.io.File
+import android.R.attr.data
+import androidx.appcompat.app.AppCompatActivity
+import android.R.attr.data
+
+
+
+
 
 class UserProfileEditFragment : Fragment() {
+    val GALLERY_REQUEST_CODE = 100
+
+    @Override
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode === AppCompatActivity.RESULT_OK) {
+            if (requestCode === GALLERY_REQUEST_CODE) {
+                val selectedImage: Uri? = data?.getData()
+
+                if (null != selectedImage) {
+                    view?.user_avatar_image?.setImageURI(selectedImage);
+                }
+            }
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+//        listAllFilesInS3()
 
         val view = layoutInflater.inflate(R.layout.fragment_user_profile_edit, container, false)
 
@@ -43,6 +76,10 @@ class UserProfileEditFragment : Fragment() {
         val updatedName = view.user_name.text
         val updatedLastName = view.user_lastName.text
         val updatedBio = view.user_bio.text
+
+        view.user_load_avatar_button?.setOnClickListener {
+            selectAvatar()
+        }
 
         view.user_profile_edit_back_button?.setOnClickListener {
             Navigation.findNavController(view).navigate(R.id.action_userProfileEditFragment_to_userProfileDetailFragment2)
@@ -66,8 +103,44 @@ class UserProfileEditFragment : Fragment() {
             { error ->
                 Log.e("Palfinder App", "Update fail", error) }
         )
+    }
 
-        Toast.makeText(context, "test...!", Toast.LENGTH_SHORT).show()
+
+
+    private fun selectAvatar() {
+//        var imageUri: Uri? = null
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "image/*"
+        val mimeTypes = arrayOf("image/jpeg", "image/png")
+        intent.putExtra(Intent.EXTRA_MIME_TYPES,mimeTypes);
+        // Launching the Intent
+//        this.resultLauncher.launch(intent)
+        startActivityForResult(intent, GALLERY_REQUEST_CODE);
+
+    }
+
+
+//    private fun uploadAvatar() {
+//        val avatar = File(appContext?.filesDir, "ExampleKey")
+//        avatar.writeText("Example file contents")
+//
+//        Amplify.Storage.uploadFile("ExampleKey", avatar,
+//            { Log.i("MyAmplifyApp", "Successfully uploaded: ${it.key}") },
+//            { Log.e("MyAmplifyApp", "Upload failed", it) }
+//        )
+//    }
+
+    private fun listAllFilesInS3() {
+//        uploadAvatar()
+
+        Amplify.Storage.list("",
+            { result ->
+                result.items.forEach { item ->
+                    Log.i("MyAmplifyApp", "Item: ${item.key}")
+                }
+            },
+            { Log.e("MyAmplifyApp", "List failure", it) }
+        )
     }
 
 }
