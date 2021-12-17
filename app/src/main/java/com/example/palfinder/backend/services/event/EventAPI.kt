5547567088167e38f7ec5bfc8b99e51dfa1ed3fb
Josué B <jbrenesr1@ucenfotec.ac.cr>
2021-12-16 18:37:58 -0600

    Participants

--- app/src/main/java/com/example/palfinder/backend/services/event/EventAPI.kt
+++ app/src/main/java/com/example/palfinder/backend/services/event/EventAPI.kt
@@ -8,6 +8,7 @@ import com.amplifyframework.api.graphql.model.ModelQuery
 import com.amplifyframework.core.Amplify
 import com.amplifyframework.core.model.query.Where
 import com.amplifyframework.datastore.generated.model.Event
+import com.amplifyframework.datastore.generated.model.EventMembers
 import com.amplifyframework.datastore.generated.model.Group
 import com.example.palfinder.backend.services.GroupAdmin
 import com.example.palfinder.backend.services.GroupService
@@ -56,10 +57,45 @@ object EventAPI {
         )
     }
 
+  /*  fun queryEventMembers(eventId: String) {
+        Log.i(TAG, "Querying Event Mmeber by Id")
+
+        Amplify.API.query(
+            ModelQuery.list(EventMembers::class.java),
+            { response ->
+                Log.i(TAG, "Queried")
+                for (eventData in response.data) {
+                    Log.i(TAG, eventData.id)
+                    // TODO should add all the groups at once instead of one by one (each add triggers a UI refresh)
+                    EventManager.addEvent(EventManager.EventModel.from(eventData))
+                }
+            },
+            { error -> Log.e(TAG, "Query failure", error) }
+        )
+    }*/
+
     fun createEvent(event : Event) {
 
         Amplify.API.mutate(
             ModelMutation.create(event),
+            { response ->
+                Log.i("Event Mnager ", "Created")
+                if (response.hasErrors()) {
+                    Log.e("Event Mnager ", response.errors.first().message)
+                } else {
+                    Log.i("Event Mnager ", "Added  Event with id: " + response.data.id)
+                }
+            },
+            { error ->
+                Log.e("Event Mnager ", "Create failed", error)
+            }
+        )
+    }
+
+    fun createEventMember(member : EventMembers) {
+
+        Amplify.API.mutate(
+            ModelMutation.create(member),
             { response ->
                 Log.i("Event Mnager ", "Created")
                 if (response.hasErrors()) {
--- app/src/main/java/com/example/palfinder/views/events/EventEditFragment.kt
+++ app/src/main/java/com/example/palfinder/views/events/EventEditFragment.kt
@@ -7,14 +7,15 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.navigation.Navigation
 import com.amazonaws.util.DateUtils
+import com.amplifyframework.auth.AuthUserAttributeKey.name
 import com.amplifyframework.core.model.temporal.Temporal
-import com.amplifyframework.datastore.generated.model.Event
-import com.amplifyframework.datastore.generated.model.EventStatus
-import com.amplifyframework.datastore.generated.model.Group
-import com.amplifyframework.datastore.generated.model.Status
+import com.amplifyframework.datastore.generated.model.*
 import com.example.palfinder.R
+import com.example.palfinder.backend.services.UserData
 import com.example.palfinder.backend.services.event.EventAPI
 import com.example.palfinder.backend.services.event.EventAPI.updateEvent
 import com.example.palfiner.backend.services.event.EventManager
@@ -27,6 +28,9 @@ import java.util.*
 class EventEditFragment : Fragment() {
 
     private val TAG = "Event Edit Fragment "
+    private var currentUser = UserData.currentUser.value!!
+    private var _eventMembers = MutableLiveData<EventMembers>()
+    var eventMembers: LiveData<EventMembers> = _eventMembers
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,6 +50,14 @@ class EventEditFragment : Fragment() {
 
                 var event = validateForm()
                 EventAPI.createEvent(event)
+
+               EventAPI.createEventMember(EventMembers.builder()
+                   .role(EventRoles.ADMIN)
+                   .user(currentUser)
+                   .event(event)
+                   .id(UUID.randomUUID().toString())
+                   .build())
+                Navigation.findNavController(view).navigate(R.id.action_creteEvent_to_groupProfile)
 
             } catch (e: IllegalStateException){
                 Log.e(TAG, "Error in Form")
